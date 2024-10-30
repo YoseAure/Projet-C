b45feb43d97f2393af958e69071787cfca4ebc56
Aurélien Collet <aurelien1722@gmail.com>
2024-10-30 14:29:35 +0100

    adjust window size to screen size

--- src/main.c
+++ src/main.c
@@ -15,7 +15,22 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Les aventures de Pestiflore", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_DisplayMode display_mode;
+    if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
+        printf("Erreur SDL_GetCurrentDisplayMode: %s\n", SDL_GetError());
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    int screen_width = display_mode.w;
+    int screen_height = display_mode.h;
+
+    float reduction_factor = 0.9;
+    int window_width = (int)(screen_width * reduction_factor);
+    int window_height = (int)(screen_height * reduction_factor);
+
+    SDL_Window *window = SDL_CreateWindow("Les aventures de Pestiflore", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_SHOWN);
     if (!window) {
         printf("Erreur SDL_CreateWindow: %s\n", SDL_GetError());
         TTF_Quit();
