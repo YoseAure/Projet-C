8e7c073c631c6bacd9da4bb3c3f426283f69dd98
Aurélien Collet <aurelien1722@gmail.com>
2024-10-30 11:29:08 +0100

    add render_text function prototype in menu header

--- include/menu.h
+++ include/menu.h
@@ -6,5 +6,6 @@
 #include <SDL2/SDL_ttf.h>
 
 void display_main_menu(SDL_Renderer *renderer);
+void render_text(SDL_Renderer *renderer, const char *text, TTF_Font *font, SDL_Color color, int x, int y);
 
 #endif
