ab1c683949cd2bb0a8502f8c3cbe447462f58e71
Aurélien Collet <aurelien1722@gmail.com>
2024-11-01 00:59:52 +0100

    add surf logo to menu options

--- include/menu.h
+++ include/menu.h
@@ -5,6 +5,11 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+typedef struct {
+    int x, y;
+} Position;
 
 void display_main_menu(SDL_Renderer *renderer);
 void render_text(SDL_Renderer *renderer, const char *text, TTF_Font *font, SDL_Color color, int x, int y);
--- src/menu.c
+++ src/menu.c
@@ -17,6 +17,15 @@ void render_text(SDL_Renderer *renderer, const char *text, TTF_Font *font, SDL_C
     SDL_DestroyTexture(texture);
 }
 
+void render_logo(SDL_Renderer *renderer, SDL_Texture *logo_texture, int option_y, int option_x) {
+    int logo_width = 50;
+    int logo_height = 50;
+
+    SDL_Rect logo_dest = {option_x - logo_width - 5, option_y + (logo_height / 4) - 20, logo_width, logo_height};
+
+    SDL_RenderCopy(renderer, logo_texture, NULL, &logo_dest);
+}
+
 void display_main_menu(SDL_Renderer *renderer) {
     TTF_Font *font = TTF_OpenFont("assets/fonts/mario-font.ttf", 24);
     TTF_Font *font_large = TTF_OpenFont("assets/fonts/mario-font.ttf", 28);
@@ -31,6 +40,14 @@ void display_main_menu(SDL_Renderer *renderer) {
     SDL_Color white = {255, 255, 255, 255};
     SDL_Color yellow = {255, 255, 0, 255};
 
+    SDL_Surface *logo_surface = IMG_Load("assets/images/surf.png");
+    if (!logo_surface) {
+        printf("Erreur IMG_Load: %s\n", IMG_GetError());
+        return;
+    }
+    SDL_Texture *logo_texture = SDL_CreateTextureFromSurface(renderer, logo_surface);
+    SDL_FreeSurface(logo_surface);
+
     const char *title = "PestyVentura";
     const char *options[] = {"Start Game", "Options", "Quit"};
     int selected = 0;
@@ -41,14 +58,13 @@ void display_main_menu(SDL_Renderer *renderer) {
     SDL_GetRendererOutputSize(renderer, &window_width, &window_height);
 
     Uint32 start_time = SDL_GetTicks();
-    int blink_interval = 1000; // Intervalle de clignotement en millisecondes
+    int blink_interval = 1500; // Intervalle de clignotement en millisecondes
 
     while (!quit) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = true;
-            }
-            else if (event.type == SDL_KEYDOWN) {
+            } else if (event.type == SDL_KEYDOWN) {
                 switch (event.key.keysym.sym) {
                     case SDLK_UP:
                         selected = (selected - 1 + 3) % 3;
@@ -82,13 +98,13 @@ void display_main_menu(SDL_Renderer *renderer) {
         int title_width = title_surface->w;
         int title_height = title_surface->h;
         int title_x = (window_width - title_width) / 2;
-        int title_y = window_height / 4; // Positionner le titre au quart de la hauteur de la fenêtre
+        int title_y = window_height / 4;
         SDL_Rect title_dest = {title_x, title_y, title_width, title_height};
         SDL_RenderCopy(renderer, title_texture, NULL, &title_dest);
         SDL_FreeSurface(title_surface);
         SDL_DestroyTexture(title_texture);
 
-        // Afficher les options du menu
+        // Afficher les options du menu et le logo à côté de l'option sélectionnée
         for (int i = 0; i < 3; ++i) {
             SDL_Color color = (i == selected) ? yellow : white;
             TTF_Font *current_font = (i == selected) ? font_large : font;
@@ -99,10 +115,15 @@ void display_main_menu(SDL_Renderer *renderer) {
             int text_width = surface->w;
             int text_height = surface->h;
             int x = (window_width - text_width) / 2;
-            int y = (window_height / 2) + (i * (text_height + 10));
+            int y = (window_height / 2) + (i * (text_height + 20));
 
             SDL_Rect dest = {x, y, text_width, text_height};
             SDL_RenderCopy(renderer, texture, NULL, &dest);
+
+            // Afficher le logo juste avant l'option sélectionnée
+            if (i == selected) {
+                render_logo(renderer, logo_texture, y, x);
+            }
 
             SDL_FreeSurface(surface);
             SDL_DestroyTexture(texture);
@@ -111,6 +132,7 @@ void display_main_menu(SDL_Renderer *renderer) {
         SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(logo_texture);
     TTF_CloseFont(font);
     TTF_CloseFont(font_large);
     TTF_CloseFont(title_font);
